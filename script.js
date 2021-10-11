b4eabd437352d85b7e102d92614723aaefeb2ad8
BenW00 <ispyb2000@gmail.com>
2021-10-10 20:49:44 -0400

    finished exercise missing bonus

---
+++ script.js
@@ -0,0 +1,62 @@
+// Question 1A
+const mcuShows = ["loki", "What If...?" ]
+
+// Question 1B
+const starWarsShows = ["The Mandalorian", "The Bad Batch"]
+
+// Question 1C
+const disneyPlusShows = ["Monsters At Work", ...mcuShows, ...starWarsShows];
+
+// Question 1D
+console.log(disneyPlusShows)
+
+// Question 2A
+const netflixMovies = {action:"Extraction", crime:"The Irishman"}
+
+// Question 2B
+const amazonPrimeMovies = {action:"The Tomorrow war", drama:"One Night In Miami"}
+
+// Question 2C
+const streamingMovies = {musical:"Hamilton", ...amazonPrimeMovies, ...netflixMovies}
+
+// Question 2D
+console.log(streamingMovies)
+
+// Question 3A
+const disneyJunior = ["Mickey Mouse Clubhouse", "Spidey and His Amazing Friends"]
+
+// Question 3B
+const [mickey, spidey] = disneyJunior
+
+// Question 3C
+console.log(mickey, spidey)
+
+// Question 4A
+const avengers = {warMachine:"James Rhodes", theHulk:"Bruce Banner"}
+
+// Question 4B
+const {warMachine, theHulk} = avengers
+
+// Question 4C
+console.log(warMachine, theHulk)
+
+// Question 4D
+const moreAvengers = {blackWidow:"Natasha Romanoff", hawkeye:"Clint Barton", ironMan:"Tony Stark"}
+
+// Question 4E
+const {blackWidow: Nat, ...others} = moreAvengers
+
+// Question 4F
+console.log(Nat, others)
+
+// BONUS 
+const bonus = {
+    first: [1, 2, 3],
+    second: [4, 5, 6],
+    third: [7, 8, 9]
+  };
+
+// Question 5A
+const all = 
+
+console.log(all)
